faab5eef2fdff6503d73afd2fff7885fc258380d
Akseli Koskinen <akseli.koskinen@eficode.com>
2017-08-26 22:21:05 +0300

    findMatchesInByteArray

--- utils.go
+++ utils.go
@@ -2,8 +2,30 @@
 
 package main
 
+import (
+  "bytes"
+)
+
 func check(e error) {
   if e != nil {
     panic(e)
   }
+}
+
+// Returns offsets/indexes from {haystack} where {needle} matches
+func findMatchesInByteArray(haystack []byte, needle []byte) []int {
+  offsets := []int{}
+  for {
+    index := bytes.Index(haystack, needle)
+    if (index == -1) {
+      break
+    }
+    if len(offsets) == 0 {
+      offsets = append(offsets, index)
+    } else {
+      offsets = append(offsets, index + offsets[len(offsets)-1] + 1)
+    }
+    haystack = haystack[index+1:]
+  }
+  return offsets
 }
