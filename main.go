2031f3d1c762a91da1667b6b1574b67d8a48c325
Akseli Koskinen <akseli.koskinen@eficode.com>
2017-08-26 23:19:12 +0300

    Overwrite feature

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
   "fmt"
   "flag"
+  "os"
 )
 
 func main() {
   fmt.Println("This is meminspector")
 
   pidPtr := flag.Int("pid", 0, "process id")
-  matchStringPtr := flag.String("s", "", "will find addresses for matches of this string in process memory data")
+  matchStringPtr := flag.String("m", "", "will find addresses for matches of this string in process memory data")
+  overwritePtr := flag.String("o", "", "string used to overwrite matches")
   flag.Parse()
   fmt.Printf("Inspecting memory for process: %d\n", *pidPtr)
 
@@ -24,5 +26,17 @@ func main() {
   fmt.Println(matches)
   for _, match := range matches {
     fmt.Printf("Match at mem address: %d\n", addrSpace[0] + int64(match))
+  }
+
+  // Overwrite matches if specified
+  if len(*overwritePtr) > 0 {
+    file, err := os.OpenFile(fmt.Sprintf("/proc/%d/mem", *pidPtr), os.O_WRONLY, 0600)
+    check(err)
+    defer file.Close()
+    for _, match := range matches {
+      _, err := file.WriteAt([]byte(*overwritePtr), addrSpace[0] + int64(match))
+      check(err)
+      fmt.Printf("Overwritten match at mem address: %d\n", addrSpace[0] + int64(match))
+    }
   }
 }
