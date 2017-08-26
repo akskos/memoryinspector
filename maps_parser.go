5298a40458320df28323ac1ad863c60c3e6366ba
Akseli Koskinen <akseli.koskinen@eficode.com>
2017-08-26 20:50:48 +0300

    fileToLines reads lines from filename to a slice

--- maps_parser.go
+++ maps_parser.go
@@ -4,12 +4,25 @@ package main
 
 import (
   "fmt"
+  "os"
+  "bufio"
 )
 
+func check(e error) {
+  if e != nil {
+    panic(e)
+  }
+}
+
+// Reads {filename} and creates a slice of its lines
 func fileToLines(filename string) []string {
-    result := []string{}
-    for i := 0; i < 10; i++ {
-      result = append(result, fmt.Sprintf("line %d", i))
-    }
-    return result
+  result := []string{}
+  file, err := os.Open(filename)
+  check(err)
+  defer file.Close()
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+    result = append(result, scanner.Text())
+  }
+  return result
 }
